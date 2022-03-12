5c83a8621858cf4bd4813a0d9a39e519e67e213e
CHANDAN RAJ <codeswithchandan@gmail.com>
2022-03-12 21:44:11 +0530

    Update customjs.js

--- js/customjs.js
+++ js/customjs.js
@@ -26,19 +26,3 @@ baguetteBox.run('.tz-gallery', {
     noScrollbars: true
 });
 
-jQuery(window).on("load",function() {
-  jQuery(window).scroll(function() {
-    var windowBottom = jQuery(this).scrollTop() + jQuery(this).innerHeight();
-    jQuery(".fade").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = jQuery(this).offset().top + jQuery(this).outerHeight();
-      
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if (jQuery(this).css("opacity")==0) {jQuery(this).fadeTo(500,1);}
-      } else { //object goes out of view (scrolling up)
-        if (jQuery(this).css("opacity")==1) {jQuery(this).fadeTo(500,0);}
-      }
-    });
-  }).scroll(); //invoke scroll-handler on page-load
-});
